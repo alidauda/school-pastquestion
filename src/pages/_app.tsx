e4767cf20302714c2b191965a3ef27289a26a8ec
alidauda <alidauda14@gmail.com>
2023-04-01 21:44:40 +0100

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/noto-sans/700.css';
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Noto Sans', sans-serif`,
+    body: `'Montserrat', sans-serif`,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
